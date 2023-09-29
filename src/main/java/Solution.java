22983da802e7658bda9f703b7aa90c19d4c162b5
alek <wait82985@gmail.com>
2023-09-29 13:15:11 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,30 @@
 public class Solution {
     public boolean isMonotonic(int[] nums) {
-        return false;
+        if (nums.length < 2) {
+            return true;
+        }
+        boolean asc = false;
+        int i = 1;
+        while (i < nums.length && nums[i] == nums[0]) {
+            i++;
+        }
+        if (i == nums.length) {
+            return true;
+        }
+        if (nums[i] - nums[0] > 0) {
+            asc = true;
+        }
+        for (; i < nums.length; i++) {
+            if (asc) {
+                if (nums[i] < nums[i - 1]) {
+                    return false;
+                }
+            } else {
+                if (nums[i] > nums[i - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
